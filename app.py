7bc6b9e1ce721ffafd0bcbc2dda1e1cd35d28070
greevashah <greeva14@gmail.com>
2019-10-14 03:05:31 +0530

    question retrieved from db

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
-from flask import Flask
-# import pymysql
+from flask import Flask, render_template
+import pymysql
 
-# def selectquery():
-#     retrieve="Select * from student"
-#     cursor.execute(retrieve)
+def selectquery():
+    retrieve="Select * from `question data` "
+    cursor.execute(retrieve)
 
-#     print('SELECT')
-#     rows= cursor.fetchall()
-#     for row in rows:
-#         print(row)
+    # print('SELECT')
+    rows= cursor.fetchall()
+    # print(rows[0][1])
+    return rows
+    # for row in rows:
+    #     print(row)
 
-# # database connection
-# connection= pymysql.connect(host="localhost",user="root",passwd="",database="gjstrial")
-# cursor=connection.cursor()
+# database connection
+connection= pymysql.connect(host="localhost",user="root",passwd="",database="berang")
+cursor=connection.cursor()
 
-# # queries
-# # retrieve="Select * from `question data` where `question no`=3;"
+# queries
+# retrieve="Select * from `question data` where `question no`=3;"
 # selectquery()
 
 # insert="Insert into student values('abc','1234','abc@gmail.com')"
@@ -23,17 +25,22 @@ from flask import Flask
 # print("After INSERT")
 # selectquery()
 
-# #commiting the connection then closing it. Otherwise the updated change is unsaved
-# connection.commit()
-# connection.close()
 
 app=Flask(__name__)
 
 @app.route('/')
-def hello():
-    return render.template('')
+def index():
+    return render_template('landing.html')
+
+@app.route('/test')
+def test():
+    rows=selectquery()
+    return render_template('test.html',value=rows)
+# Sends all rows as value, possible due to render template
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=5000, debug=True)
 
-    
+#commiting the connection then closing it. Otherwise the updated change is unsaved
+connection.commit()
+connection.close()
