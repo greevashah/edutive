7a559fce24af1af8d5c2649484d8272f19bc683c
greevashah <greeva14@gmail.com>
2019-10-13 15:27:31 +0530

    initial set up

--- app.py
+++ app.py
@@ -1,35 +1,35 @@
-# from flask import Flask
-import pymysql
+from flask import Flask
+# import pymysql
 
-def selectquery():
-    retrieve="Select * from student"
-    cursor.execute(retrieve)
+# def selectquery():
+#     retrieve="Select * from student"
+#     cursor.execute(retrieve)
 
-    print('SELECT')
-    rows= cursor.fetchall()
-    for row in rows:
-        print(row)
+#     print('SELECT')
+#     rows= cursor.fetchall()
+#     for row in rows:
+#         print(row)
 
-# database connection
-connection= pymysql.connect(host="localhost",user="root",passwd="",database="gjstrial")
-cursor=connection.cursor()
+# # database connection
+# connection= pymysql.connect(host="localhost",user="root",passwd="",database="gjstrial")
+# cursor=connection.cursor()
 
-# queries
-# retrieve="Select * from `question data` where `question no`=3;"
-selectquery()
+# # queries
+# # retrieve="Select * from `question data` where `question no`=3;"
+# selectquery()
 
-insert="Insert into student values('abc','1234','abc@gmail.com')"
-cursor.execute(insert)
-print("After INSERT")
-selectquery()
+# insert="Insert into student values('abc','1234','abc@gmail.com')"
+# cursor.execute(insert)
+# print("After INSERT")
+# selectquery()
 
-# app=FLASK(__name__)
+app=FLASK(__name__)
 
-# @app.route('/')
-# def hello():
-#     return 'Hi'
+@app.route('/')
+def hello():
+    return 'Hi'
 
-# if __name__ == "__main__":
-#     app.run()
+if __name__ == "__main__":
+    app.run()
 
     
