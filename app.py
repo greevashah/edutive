d3b6137174da4cc9839084117f1090ad7c72f34c
greevashah <greeva14@gmail.com>
2019-10-13 15:22:35 +0530

    initial setup

--- app.py
+++ app.py
@@ -1,35 +1,39 @@
 # from flask import Flask
 import pymysql
 
-def selectquery():
-    retrieve="Select * from student"
-    cursor.execute(retrieve)
+# def selectquery():
+#     retrieve="Select * from student"
+#     cursor.execute(retrieve)
 
-    print('SELECT')
-    rows= cursor.fetchall()
-    for row in rows:
-        print(row)
+#     print('SELECT')
+#     rows= cursor.fetchall()
+#     for row in rows:
+#         print(row)
 
-# database connection
-connection= pymysql.connect(host="localhost",user="root",passwd="",database="gjstrial")
-cursor=connection.cursor()
+# # database connection
+# connection= pymysql.connect(host="localhost",user="root",passwd="",database="gjstrial")
+# cursor=connection.cursor()
 
-# queries
-# retrieve="Select * from `question data` where `question no`=3;"
-selectquery()
+# # queries
+# # retrieve="Select * from `question data` where `question no`=3;"
+# selectquery()
 
-insert="Insert into student values('abc','1234','abc@gmail.com')"
-cursor.execute(insert)
-print("After INSERT")
-selectquery()
+# insert="Insert into student values('abc','1234','abc@gmail.com')"
+# cursor.execute(insert)
+# print("After INSERT")
+# selectquery()
 
-# app=FLASK(__name__)
+# #commiting the connection then closing it. Otherwise the updated change is unsaved
+# connection.commit()
+# connection.close()
 
-# @app.route('/')
-# def hello():
-#     return 'Hi'
+app=Flask(__name__)
 
-# if __name__ == "__main__":
-#     app.run()
+@app.route('/')
+def hello():
+    return render.template('')
+
+if __name__ == "__main__":
+    app.run()
 
     
