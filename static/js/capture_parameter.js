bc3ff961bf061dad4c29b6bc6ce0003798a280c7
greevashah <greeva14@gmail.com>
2019-12-16 13:58:22 +0530

    marked answer stays feature and time per question feature partially executed

--- static/js/capture_parameter.js
+++ static/js/capture_parameter.js
@@ -1,10 +1,18 @@
 var questions=new Array(15); // Array of Random Question numbers
 var answers=new Array(15);  //Array of Whether Answers marked are coorect(1) or not(0)
-var elapsedtime = new Array(15);
+var elapsedtime = new Array(15);    //Array to store time taken in each question
+
+var Default_val=0;
+elapsedtime.fill(Default_val);
+
 var timeTaken=30;
 var timerFun;
 var row=undefined;
+
 window.onload= timer();
+var startTime=undefined;
+var finishTime=undefined;
+
 // Function which ensures correct question is selected
 
 //row refers to the data sent by flask, in our case it is the entire question table
@@ -24,8 +32,10 @@ function initialise(x){
         }
     }
     alert(questions);
+    alert(elapsedtime);
 }
 
+//Display the question the dynamically and get cookie to mark previously marked answer
 function renderQuestion(a) {
     // alert(x);
     ques = document.getElementById("question-data");
@@ -37,61 +47,52 @@ function renderQuestion(a) {
     // So row[x-1][1], refers to the (x-1)th row and 1st indexed column
     ques.innerHTML = "<h5 class='card-title' id='question-num'>Question No. " + a + "</h5>";
     ques.innerHTML += "<p class='card-text' id='question-content'>" + row[x - 1][1] + "</p>";
-    ques.innerHTML += "<input type='radio' name='option' value='1'>" + row[x - 1][4] + "<br><br>";
-    ques.innerHTML += "<input type='radio' name='option' value='2'>" + row[x - 1][5] + "<br><br>";
-    ques.innerHTML += "<input type='radio' name='option' value='3'>" + row[x - 1][6] + "<br><br>";
-    ques.innerHTML += "<input type='radio' name='option' value='4'>" + row[x - 1][7] + "<br><br>";
+    ques.innerHTML += "<input id='option1' type='radio' name='option' value='1'>" + row[x - 1][4] + "<br><br>";
+    ques.innerHTML += "<input id='option2'type='radio' name='option' value='2'>" + row[x - 1][5] + "<br><br>";
+    ques.innerHTML += "<input id='option3'type='radio' name='option' value='3'>" + row[x - 1][6] + "<br><br>";
+    ques.innerHTML += "<input id='option4'type='radio' name='option' value='4'>" + row[x - 1][7] + "<br><br>";
     
+    var prev_ans=getCookie("Answer"+a);
+    //alert("prev ans "+ prev_ans);
+    if(prev_ans){
+        document.getElementById('option'+prev_ans).checked=true;
+    }
+    
+    startTime=Date.now()/1000; 
     //document.getElementById("question-num").addEventListener("load", qtimer);
 }
 
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(' ');
+    //alert("Cookie is "+ca);
 
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
 
-//    function SaveForm(){
-//     setCookie("Answer", storeAnswer(),30);
-//    }
-
-//    function SetAnsValue(ansValue) {
-//     var ansRadio = document.getElementsByName('option');
-//     for(var i = 0; i <ansRadio.length; i++){
-//         if(ansRadio[i].value == ansValue){
-//            ansRadio[i].checked = true;
-//         }
-//     }
-//   }
-
-//   function LoadForm(){
-//     SetAnsValue( getCookie("Answer") );
-//   } 
-
-//   function getCookie(name) {
-//     var nameEQ = name + "=";
-//     var ca = document.cookie.split(';');
-//     for (var i = 0; i < ca.length; i++) {
-//         var c = ca[i];
-//         while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-//         if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-//     }
-//     return null;
-// }
-
-//   function setCookie(name, value, minutes) { // from http://www.quirksmode.org/js/cookies.html
-//     if (minutes) {
-//         var date = new Date();
-//         date.setTime(date.getTime() + (minutes * 60 * 1000));
-//         var expires = "; expires=" + date.toGMTString();
-//     } else var expires = "";
-//     document.cookie = name + "=" + value + expires + "; path=/";
-// }
+function setCookie(name, value, minutes) {
+    if (minutes) {
+        var date = new Date();
+        date.setTime(date.getTime() + (minutes * 60 * 1000));
+        var expires = " expires=" + date.toGMTString();
+    } else var expires = "";
+    document.cookie = name + "=" + value + expires + " path=/";
+}
  
 
-//Function which stores answer for each question every time the answer is saved
+//Function which stores answer for each question every time the answer is saved and sets the cookie
 function storeAnswer(){
 
     //var row = {{ value }};
     var qno=document.getElementById('question-num');
     //Question No. 12
-    var qnum = questions[parseInt(qno.innerText.substring(13))]; //Substring "12" converted to 12; 
+    var qnum_cur=parseInt(qno.innerText.substring(13));
+    var qnum = questions[qnum_cur-1]; //Substring "12" converted to 12; 
     //substring(13) as from 'Question No. 3' we want from 13th char onwards
     //then find the corresponding random question number as, questions[...the whole code to find the corresponding question...]
 
@@ -102,23 +103,36 @@ function storeAnswer(){
     var ele = document.getElementsByName('option');
     var ansValue; 
     for(i = 0; i < ele.length; i++) { 
-    if(ele[i].checked){
-        ansValue = ele[i].value;
-        if(ele[i].value == ans){
-        //alert("Correct Answer");
-        answers[qnum-1]=1;
+        if(ele[i].checked){
+            ansValue = ele[i].value;
+            if(ele[i].value == ans){
+            //alert("Correct Answer");
+            answers[qnum-1]=1;
+            }
+            else{
+            //alert("Wrong Answer");
+            answers[qnum-1]=0;
+            }
         }
-        else{
-        //alert("Wrong Answer");
-        answers[qnum-1]=0;
-        }
-    }
     }
 
-    return ansValue;
+    finishTime=Date.now()/1000;
+    elapsedtime[qnum_cur-1] += Math.floor(finishTime-startTime);
+    startTime=finishTime;
+    alert(answers);
+    alert(elapsedtime);
+    setCookie("Answer"+qnum_cur,ansValue,30);
 }
 
+function deleteAllCookies(){
+    var cookies = document.cookie.split(";");
+    for (var i = 0; i < cookies.length; i++)
+      deleteCookie(cookies[i].split("=")[0]);
+}
 
+function deleteCookie(name){
+    setCookie(name,"",-1);
+}
 
 //Function which checks the total number of correct answers at the end of the test
 function checkAnswers(){
@@ -143,6 +157,7 @@ function checkAnswers(){
     var diff=(30*60)-seconds;
 
     alert("Time Taken by you is "+ toTimeString(diff));
+    deleteAllCookies();
 }
 
 function toTimeString(seconds){
@@ -170,6 +185,3 @@ function timer(){
     }
     },1000);
 }
-
-
-
