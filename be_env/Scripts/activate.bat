28ed738a9158c8c39b844c5f445a2c2feb376a31
smita <smita.patil@vorbicon.com>
2019-12-19 01:42:15 +0530

    update

--- be_env/Scripts/activate.bat
+++ be_env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=F:\Greeva's Data\BE Project\berang\be_env"
+set "VIRTUAL_ENV=C:\berang\be_env"
 
 if not defined PROMPT (
     set "PROMPT=$P$G"
