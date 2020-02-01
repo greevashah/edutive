e35324b293987ffc99a0524e32e8dcfe4d412025
greevashah <greeva14@gmail.com>
2020-02-01 23:54:38 +0530

    adding parameters acc to topic done

--- app.py
+++ app.py
@@ -47,8 +47,16 @@ def selecttopic4():
 
 def insertDataset():
     for i in range(len(qnum)):
-        insert="INSERT INTO `dataset`(qno, correctness, tpque, optionchanges, tptopic, tptest, topic, difficulty) VALUES("+str(qnum[i])+","+str(ans[i])+","+str(elapt[i])+","+str(optch[i])+",0,'"+str(totaltime)+"','"+topic[i]+"','"+difficulty[i]+"')"
+        insert="INSERT INTO `dataset`(qno, correctness, tpque, optionchanges, tptopic, tptest, topic, difficulty) VALUES("+str(qnum[i])+","+str(ans[i])+","+str(elapt[i])+","+str(optch[i])+","+str(timept[topic[i]])+",'"+str(totaltime)+"','"+topic[i]+"','"+difficulty[i]+"')"
         # insert="INSERT INTO `dataset`(qno, correctness, tpque, optionchanges, tptopic, tptest, topic, difficulty) VALUES(%d,%d,%d,%d,%d,%s,%s,%s)"
+        cursor.execute(insert)
+        connection.commit()
+        # print(str(insert))
+
+def insertTopicDataset():
+    for i in topicwise.keys():
+        insert="INSERT INTO `topicdataset`(topic, correctness, tpque, optionchanges, tptopic, tptest) VALUES('"+i+"','"+str(topicwise[i][0])+"','"+str(topicwise[i][1])+"','"+str(topicwise[i][2])+"','"+str(timept[i])+"','"+str(totaltime)+"')"
+        # insert="INSERT INTO `topicdataset`(qno, correctness, tpque, optionchanges, tptopic, tptest, topic, difficulty) VALUES(%d,%d,%d,%d,%d,%s,%s,%s)"
         cursor.execute(insert)
         connection.commit()
         print(str(insert))
@@ -57,14 +65,100 @@ def insertDataset():
 def computeRows():
     # print(type(qnum))
     # print(type(rows))
-    global topic,difficulty
+    global topic,difficulty, timept, topicQ
     topic=[]
     difficulty=[]
+    timept=dict()        #dictionary of time per topic; (Topic name)=>(Time) {TW:0,TSD:0,SI:0,PPL:0}
+    timept['TSD']= 0
+    timept['TW']= 0
+    timept['SI']= 0
+    timept['PPL']= 0
+    topicQ=dict()       #dictionary which stores list of question numbers of a particular topic,[5,30,46,100] {TW:[],TSD:[],SI:[],PPL:[]}
+    topicQ['TSD']=[]
+    topicQ['TW']=[]
+    topicQ['SI']=[]
+    topicQ['PPL']=[]
+    c=0
     for q in qnum:
         difficulty.append(rows[q-1][9])     #list of string
-        topic.append(rows[q-1][10])
+        tmp=rows[q-1][10]
+        topic.append(tmp)
+        if(tmp =='TW'):
+            topicQ['TW'].append(q)
+            timept['TW'] +=elapt[c]
+        elif(tmp=='TSD'):
+            topicQ['TSD'].append(q)
+            timept['TSD'] += elapt[c]
+        elif(tmp=='SI'):
+            topicQ['SI'].append(q)
+            timept['SI'] += elapt[c]
+        elif(tmp=='PPL'):
+            topicQ['PPL'].append(q)
+            timept['PPL'] += elapt[c]
+        c= c+1
     print(topic)
     print(difficulty)
+    print(topicQ)
+    print(timept)
+
+def computeTopicwise():
+    global topicwise
+    topicwise=dict()
+    topicwise['TSD']=[0]*3
+    topicwise['TW']=[0]*3
+    topicwise['SI']=[0]*3
+    topicwise['PPL']=[0]*3
+
+    l1= len(topicQ['TSD'])
+    l2= len(topicQ['TW'])
+    l3= len(topicQ['SI'])
+    l4= len(topicQ['PPL'])
+
+    c=0
+    qattempt=[0]*4
+    for q in qnum:
+        tmp=rows[q-1][10]
+        if(tmp=='TSD'):
+            if(ans[c] != -1):
+                topicwise['TSD'][0] += ans[c]/l1
+                qattempt[0]+=1
+            else:
+                topicwise['TSD'][0] += 0
+            topicwise['TSD'][1] += elapt[c]/l1
+            topicwise['TSD'][2] += optch[c]
+
+        elif(tmp =='TW'):
+            if(ans[c] != -1):
+                topicwise['TW'][0] += ans[c]/l2
+                qattempt[1]+=1
+            else:
+                topicwise['TW'][0] += 0
+            topicwise['TW'][1] += elapt[c]/l2
+            topicwise['TW'][2] += optch[c]
+
+        elif(tmp=='SI'):
+            if(ans[c] != -1):
+                topicwise['SI'][0] += ans[c]/l3
+                qattempt[2]+=1
+            else:
+                topicwise['SI'][0] += 0
+            topicwise['SI'][1] += elapt[c]/l3
+            topicwise['SI'][2] += optch[c]
+
+        elif(tmp=='PPL'):
+            if(ans[c] != -1):
+                topicwise['PPL'][0] += ans[c]/l4
+                qattempt[3]+=1
+            else:
+                topicwise['PPL'][0] += 0
+            topicwise['PPL'][1] += elapt[c]/l4
+            topicwise['PPL'][2] += optch[c]
+        c=c+1
+    topicwise['TSD'][2] /= qattempt[0] if qattempt[0] > 0 else 1
+    topicwise['TW'][2] /= qattempt[1] if qattempt[1] > 0 else 1
+    topicwise['SI'][2] /= qattempt[2] if qattempt[2] > 0 else 1
+    topicwise['PPL'][2] /= qattempt[3] if qattempt[3] > 0 else 1
+    print(topicwise)
 
 def convertToIntList(arr):
     result=[]
@@ -113,6 +207,8 @@ def get_data():
     # print(type(arr4))
     computeRows()
     insertDataset()
+    computeTopicwise()
+    insertTopicDataset()
     return 200
 
 if __name__ == "__main__":
