02965aff9019d40995eab522b04c4cd648426fac
ninad1398 <ninadmapara@gmail.com>
2020-02-12 16:20:33 +0530

    database change hosting final

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pymysql,os
 
 
 # database connection
-connection= pymysql.connect(host="sql12.freemysqlhosting.net",user="sql12322245",passwd="PfmNYfbQGj",database="sql12322245")
+connection= pymysql.connect(host="remotemysql.com",user="tthTEMzAku",passwd="VDy1GLuLuT",database="tthTEMzAku")
 cursor=connection.cursor() 
 
 
