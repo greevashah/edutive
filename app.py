78f116f3ef5766ed3c70e10de62def41d39cf8d9
ninad1398 <ninadmapara@gmail.com>
2020-04-28 17:11:45 +0530

    register form

--- app.py
+++ app.py
@@ -571,7 +571,8 @@ if __name__ == "__main__":
     app.jinja_env.auto_reload = True
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='127.0.0.1', port=port)
+    # app.run(host='0.0.0.0', port=port)
 
 #commiting the connection then closing it. Otherwise the updated change is unsaved
 connection.commit()
