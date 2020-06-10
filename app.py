2e9051add3d5b1519b34b46b4a6ee15b9efd2e0a
ninad1398 <ninadmapara@gmail.com>
2020-06-10 18:55:47 +0530

    Performance bugs Resolved

--- app.py
+++ app.py
@@ -41,7 +41,3 @@ if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     # app.run(host='0.0.0.0', port=port)
     app.run(host='127.0.0.1', port=port, debug= True)
-
-#commiting the connection then closing it. Otherwise the updated change is unsaved
-connection.commit()
-connection.close()
